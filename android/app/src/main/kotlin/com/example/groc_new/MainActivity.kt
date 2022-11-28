a34b7d5d039859a2df7ee9eaeaa9f96009e88652
Tauqeer <2roshankhan6@gmail.com>
2022-11-28 05:44:49 +0000

    first

---
+++ android/app/src/main/kotlin/com/example/groc_new/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.groc_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:multi_store_app/main_screens/customer_home.dart';
+import 'package:multi_store_app/main_screens/supplier_home.dart';
+import 'package:multi_store_app/main_screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'auth/customer_signup.dart';
+
+void main()async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/welcome_screen',
+      routes: {
+        '/welcome_screen': (context) => const WelcomeScreen(),
+        '/customer_home': (context) => const CustomerHomeScreen(),
+        '/supplier_home': (context) => const SupplierHomeScreen(),
+        '/customer_sign':(context)=> const CustomerRegister(),
+      },
+    );
+  }
+}
